6a2e42eb9519b4df503311cca26fc9d9ec800d51
Calco Dev <calcopoddev@gmail.com>
2023-01-06 15:06:43 +0200

    Trail renderer now has an Emitting property.

--- scripts/components/TrailRendererComponent.cs
+++ scripts/components/TrailRendererComponent.cs
@@ -6,6 +6,7 @@ public partial class TrailRendererComponent : Line2D
 {
 	[Export] public int Length { get; set; } = 100;
 	[Export] public float Delay { get; set; } = 0.1f;
+	[Export] public bool Emitting { get; set; } = true;
 
 	[Export] private CustomTimerComponent _timer;
 
@@ -22,14 +23,17 @@ public partial class TrailRendererComponent : Line2D
 
 	public override void _Process(double delta)
 	{
+		_timer.Update((float) delta);
+		
 		GlobalPosition = Vector2.Zero;
 		GlobalRotation = 0f;
 		
-		_timer.Update((float) delta);
 		if (_timer.HasFinished())
 		{
 			_timer.SetTime(Delay);
-			AddPoint(_parent.GlobalPosition + _offset);
+			
+			if (Emitting)
+				AddPoint(_parent.GlobalPosition + _offset);
 			
 			if (GetPointCount() > Length)
 				RemovePoint(0);
