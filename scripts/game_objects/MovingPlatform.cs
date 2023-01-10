c8ec93267337352f931e2434074a69cf2b78af77
Calco Dev <calcopoddev@gmail.com>
2023-01-10 17:05:43 +0200

    Played around with lights. Idunno what is going on. Failed to fix trail renderer >:(.

--- scripts/game_objects/MovingPlatform.cs
+++ scripts/game_objects/MovingPlatform.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using Godot;
 using Incandescent.Components;
 
@@ -6,50 +6,50 @@ namespace Incandescent.GameObjects;
 
 public partial class MovingPlatform : Node2D
 {
-    [ExportGroup("Refs")]
-    [Export] private SolidComponent _solid;
+	[ExportGroup("Refs")]
+	[Export] private SolidComponent _solid;
 
-    [ExportGroup("Settings")]
-    [Export] private Node _path;
-    [Export] private float _duration = 1f;
-    [Export] private float _delay = 0.5f;
+	[ExportGroup("Settings")]
+	[Export] private Node _path;
+	[Export] private float _duration = 1f;
+	[Export] private float _delay = 0.5f;
 
-    private List<Node2D> _points;
-    private int _currentPoint = -1;
+	private List<Node2D> _points;
+	private int _currentPoint = -1;
 
-    private Tween _tween;
-    private Vector2 _targetPos;
+	private Tween _tween;
+	private Vector2 _targetPos;
 
-    public override void _Ready()
-    {
-        base._Ready();
+	public override void _Ready()
+	{
+		base._Ready();
 
-        int cnt = _path.GetChildCount();
-        _points = new List<Node2D>(cnt);
-        for (int i = 0; i < cnt; i++)
-            _points.Add(_path.GetChild<Node2D>(i));
+		int cnt = _path.GetChildCount();
+		_points = new List<Node2D>(cnt);
+		for (int i = 0; i < cnt; i++)
+			_points.Add(_path.GetChild<Node2D>(i));
 
-        _tween = CreateTween();
+		_tween = CreateTween();
 
-        _targetPos = _points[0].Position;
-        for (int i = 0; i < cnt; i++)
-        {
-            int next = (i + 1) % cnt;
-            _tween
-                .TweenProperty(this, "_targetPos", _points[next].GlobalPosition, _duration)
-                .SetEase(Tween.EaseType.InOut)
-                .SetTrans(Tween.TransitionType.Expo)
-                .SetDelay(_delay);
-        }
-        _tween.SetLoops();
-        _tween.Play();
-    }
+		_targetPos = _points[0].Position;
+		for (int i = 0; i < cnt; i++)
+		{
+			int next = (i + 1) % cnt;
+			_tween
+				.TweenProperty(this, "_targetPos", _points[next].GlobalPosition, _duration)
+				.SetEase(Tween.EaseType.InOut)
+				.SetTrans(Tween.TransitionType.Expo)
+				.SetDelay(_delay);
+		}
+		_tween.SetLoops();
+		_tween.Play();
+	}
 
-    public override void _Process(double delta)
-    {
-        _solid.MoveX(_targetPos.x - (GlobalPosition.x + _solid.Remainder.x));
-        _solid.MoveY(_targetPos.y - (GlobalPosition.y + _solid.Remainder.y));
+	public override void _Process(double delta)
+	{
+		_solid.MoveX(_targetPos.x - (GlobalPosition.x + _solid.Remainder.x));
+		_solid.MoveY(_targetPos.y - (GlobalPosition.y + _solid.Remainder.y));
 
-        GlobalPosition = _solid.GlobalPosition;
-    }
+		GlobalPosition = _solid.GlobalPosition;
+	}
 }
---
+++ scripts/utils/LightGrow.cs
@@ -0,0 +1,23 @@
+using System;
+using Godot;
+
+namespace Incandescent.Utils;
+
+public partial class LightGrow : PointLight2D
+{
+    private float _a;
+    private float _init;
+
+    public override void _Ready()
+    {
+        _init = TextureScale;
+    }
+
+    public override void _Process(double delta)
+    {
+        _a = Mathf.Sin(Time.GetTicksMsec() * 0.001f) * 0.05f;
+
+        TextureScale = _init + _a;
+        RotationDegrees += 0.5f;
+    }
+}
